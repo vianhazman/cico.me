23439abf1f16d662976d98d09873a9b835183f3f
vianhazman <vianhazman@gmail.com>
2019-04-21 04:23:32 +0700

    Updates

---
+++ precache-manifest.60dbd2c914bfd5f4de09aaee2b079f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f943f8935d90078e55d79ea2133ecb4",
+    "url": "/cico.me/static/media/hero.7f943f89.gif"
+  },
+  {
+    "revision": "af9c6cfbc097f722d864",
+    "url": "/cico.me/static/js/runtime~main.cc993a8d.js"
+  },
+  {
+    "revision": "a2cb79cb65ad1c91c26e",
+    "url": "/cico.me/static/js/main.672d8cfc.chunk.js"
+  },
+  {
+    "revision": "3d7c11851aceaba2254d",
+    "url": "/cico.me/static/js/2.77652ad5.chunk.js"
+  },
+  {
+    "revision": "a2cb79cb65ad1c91c26e",
+    "url": "/cico.me/static/css/main.c074c19b.chunk.css"
+  },
+  {
+    "revision": "f766c0e518728aaaa5e201f1673da020",
+    "url": "/cico.me/index.html"
+  }
+];
